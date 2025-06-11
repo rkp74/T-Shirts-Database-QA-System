e3e03a4939392acb1c9fa2ed570afb5903fc1511
Rajan Kumar <rkp74r@MacBook-Air.local>
2025-06-11 13:28:24 +0530

    Whole Code

--- langchain_helper.py
+++ langchain_helper.py
@@ -17,7 +17,7 @@ load_dotenv()  # take environment variables from .env (especially openai api key
 
 def get_few_shot_db_chain():
     db_user = "root"
-    db_password = "rajan123"
+    db_password = "....."
     db_host = "localhost"
     db_name = "woodpecker_tshirts"
 
